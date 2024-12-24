5cc0dd29939786996d35fdd769ec91240cc2a701
Pushpendra Patel <134173726+Pushpendra6376@users.noreply.github.com>
2024-12-24 22:33:11 +0530

    css basics uploaded

---
+++ cssbasicspart1/cssBasicsPart1.js
@@ -0,0 +1,24 @@
+// Select the form element and apply styles
+const form = document.querySelector('form');
+form.style.fontFamily = 'Arial';
+form.style.backgroundColor = 'gray';
+form.style.padding = '20px';
+form.style.borderRadius = '20px';
+
+// Select the label element and apply styles
+const label = document.querySelector('label');
+label.style.fontWeight = 'bold';
+label.style.fontSize = '20px';
+label.style.color = 'blue';
+
+// Select the input element and apply styles
+const input = document.querySelector('input');
+input.style.fontStyle = 'italic';
+input.style.marginBottom = '20px';
+input.style.height = '30px';
+
+// Select the button element and apply styles
+const button = document.querySelector('button');
+button.style.backgroundColor = 'yellow';
+button.style.width = '225px';
+button.style.border = '1px solid yellow';
